3f63598efee1e7da6b5f783be8fb7e3ad2d8bc0a
Michal Konecny <mkonecny@redhat.com>
2026-01-20 12:55:18 +0100

    Reformat codebase using black v26
    Reformat whole codebase with latest black.
    
    Signed-off-by: Michal Konecny <mkonecny@redhat.com>

--- anitya_schema/project_messages.py
+++ anitya_schema/project_messages.py
@@ -14,6 +14,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """The schema for project-related messages sent by Anitya."""
+
 import warnings
 
 from .base import AnityaMessage, ANITYA_URL
